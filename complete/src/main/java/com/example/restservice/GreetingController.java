c406375aaccebdb7c73c82649a6d98b0c77a0461
QueenKimm <gudarzikimia@gmail.com>
2024-02-09 15:32:16 -0500

    Greeting cnotroller

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -3,6 +3,7 @@ package com.example.restservice;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,14 @@ public class GreetingController {
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "Kir") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
+
+	@PostMapping("/success")
+	public Success success(){
+		return new Success("Done");
+	}
+
 }
